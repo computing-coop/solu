370c84616331feee6d4f73386fb2b92993939d33
John W. Fail <fail@cenotaph.org>
2015-02-27 14:34:02 +0200

    ckeditor assets in compilation

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -5,6 +5,7 @@ Ckeditor.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require "ckeditor/orm/mongoid"
+  config.assets_languages = ['en']
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
