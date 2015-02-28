462bec59d0497b3f82df22b63bf6f40739941884
John W. Fail <fail@cenotaph.org>
2015-02-28 17:26:19 +0200

    smaller header and partner logos

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -14,10 +14,15 @@ class Partner
   field :postcode, type: String
   field :description, type: String
   field :logo, type: String
-  field :logo_file_size, type: String
+  field :logo_size, type: String
   field :logo_width, type: Integer
   field :logo_height, type: Integer
   field :logo_content_type, type: String
+  field :hmlogo, type: String
+  field :hmlogo_size, type: String
+  field :hmlogo_width, type: Integer
+  field :hmlogo_height, type: Integer
+  field :hmlogo_content_type, type: String
   
   field :coordinates, :type => Array
   
@@ -32,6 +37,9 @@ class Partner
   index({ name: 1 }, { unique: true, drop_dups: true, name: "name_index" })
   
   mount_uploader :logo, ImageUploader
+  mount_uploader :hmlogo, ImageUploader
+  
+  before_save :get_logo_metadatas
   
   include Geocoder::Model::Mongoid
   geocoded_by :full_address
@@ -39,6 +47,26 @@ class Partner
   
   def full_address
     [address1, address2, [postcode, city].compact.join(' '), country].compact.join(', ')
+  end
+  
+  def get_logo_metadatas
+
+    if logo.present? && logo_changed?
+      if logo.file.exists?
+        self.logo_content_type = logo.file.content_type
+        self.logo_size = logo.file.size
+        self.logo_width, self.logo_height = `identify -format "%wx%h" #{logo.file.path}`.split(/x/)
+      end
+    end
+
+    if hmlogo.present? && hmlogo_changed?
+      if hmlogo.file.exists?
+        self.hmlogo_content_type = hmlogo.file.content_type
+        self.hmlogo_size = hmlogo.file.size
+        self.hmlogo_width, self.hmlogo_height = `identify -format "%wx%h" #{hmlogo.file.path}`.split(/x/)
+      end
+    end
+    
   end
   
 end
