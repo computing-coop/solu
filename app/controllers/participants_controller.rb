76d4308755d0eb1261786bac90397a3dc67b29ff
John W. Fail <fail@cenotaph.org>
2015-05-12 12:05:42 +0300

    participants controller and view

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,13 @@
+class ParticipantsController < ApplicationController
+  
+  def index
+    if @site.nil?
+      redirect_to '/'
+    else
+      @participants = @site.symposium.groups.map{|x| x.participants.approved }.flatten.sort_by{|x| x.name }
+      set_meta_tags title: @site.symposium.name + ": Participants"
+      render layout: @site.layout
+    end
+  end
+  
+end
