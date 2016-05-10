4facb044b76cdf377299419ec0bf1a2018c90deb
John W. Fail <john@pixelache.ac>
2016-05-10 13:37:02 +0300

    artist view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,21 @@
 class ArtistsController < ApplicationController
   
+  
+  def index
+
+    if @activity.nil?
+      @artists = Artist.order(start_at: :desc)
+      set_meta_tags title: "Artists"
+     
+    else
+      @artists = @activity.artists.order(title: :asc)
+      set_meta_tags title: @activity.name + ": Artists"
+
+    end
+    render layout: @site.layout
+  end
+  
+  
   def show
     @artist = Artist.find(params[:id])
     set_meta_tags title: @artist.name
