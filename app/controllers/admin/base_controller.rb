3d59fe7590ae690cf87926b3f3c240f69931cce4
John W. Fail <john@pixelache.ac>
2016-11-01 17:26:33 +0200

    make it work in production

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,7 +7,7 @@ class Admin::BaseController < ApplicationController
   check_authorization
   skip_before_filter :verify_authenticity_token 
   
-  skip_before_filter :require_no_authentication
+  # skip_before_filter :require_no_authentication
   load_and_authorize_resource
  
   def check_permissions
