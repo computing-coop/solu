853de8c25f9023e7b3a8586bd4313788f883b0a4
John W. Fail <john@pixelache.ac>
2017-03-16 13:21:03 +0200

    turn off signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable #, :validatable
+  devise :database_authenticatable, #:registerable,
+         :confirmable, :recoverable, :rememberable, :trackable #, :validatable
 
   ## Database authenticatable
   field :email,              type: String, default: ""
