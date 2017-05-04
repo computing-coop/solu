8005cec6c51d487d0cfeebbca171281b3a38b026
John W. Fail <john@pixelache.ac>
2017-05-04 23:14:33 +0300

    put calls on frontpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -57,7 +57,7 @@ class HomeController < ApplicationController
       @posts = Post.published.order(published_at: :desc).limit(8)
       @about = @node.pages.find('about-the-bioart-society') rescue nil
       @ars = @node.pages.find('ars-bioarctica-front') rescue nil
-      @calls = Project.find('ars-bioarctica').calls.active
+      @calls = Project.friendly.find('ars-bioarctica').calls.active
       @projects = Project.published
     end
   end
