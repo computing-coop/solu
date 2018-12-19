89a78bd9ca2e7dd40168cc471392193c04786571
John W. Fail <john@pixelache.ac>
2018-12-19 16:38:57 +0200

    fixing rollbar bug #39

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -2,15 +2,22 @@ class PartnersController < ApplicationController
   respond_to :html
   
   def index
-    @partners = Partner.by_node(@node).sort_by(&:created_at)
-    set_meta_tags title: 'Partners'
-    respond_with @partners
+    if @node.name =='bioart'
+      redirect_to host: 'hybridmatters.net'
+    else
+      @partners = Partner.by_node(@node).sort_by(&:created_at)
+      set_meta_tags title: 'Partners'
+      respond_with @partners
+    end
   end
-  
+ 
   def show
-    @partner = Partner.find(params[:id])
-    set_meta_tags title: @partner.name
-    respond_with @partner
+    if @node.name =='bioart'
+      redirect_to host: 'hybridmatters.net'
+    else    
+      @partner = Partner.find(params[:id])
+      set_meta_tags title: @partner.name
+      respond_with @partner
+    end
   end
-  
 end
