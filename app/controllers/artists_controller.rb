c39cddccddd62021a276e6d4cf2e1e8f78f734f9
John W. Fail <john@pixelache.ac>
2018-12-19 15:37:36 +0200

    artist bug fix hopefully

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,15 +1,13 @@
 class ArtistsController < ApplicationController
-  
-  
+ 
   def index
     if @node.name == 'bioart'
       @artists = Artist.all.order(alphabetical_name: :asc)
       render template: 'residencies/index'
     else
       if @activity.nil?
-        @artists = Artist.order(alphabetical_name: :asc)
-        set_meta_tags title: "Artists"
-       
+        @artists = Work.all.map(&:artists).flatten.sort_by(&:alphabetical_name).uniq
+        set_meta_tags title: 'Artists'     
       else
         @artists = @activity.works.map(&:artists).flatten.sort_by(&:alphabetical_name).uniq
         set_meta_tags title: @activity.name + ": Artists"
@@ -18,16 +16,18 @@ class ArtistsController < ApplicationController
       render layout: @site.layout
     end
   end
-  
-  
+
   def show
     @artist = Artist.find(params[:id])
     set_meta_tags title: @artist.name
     if @node.name == 'bioart'
       render template: 'residencies/show'
     else
-      render layout: @site.layout
+      if @artist.works.empty?
+        redirect_to artist_url(@artist, host: Rails.env.development? ? 'bioartsociety.local' : Node.find('bioart').subdomains + '.fi')
+      else
+        render layout: @site.layout
+      end
     end
   end
-  
 end
