4655b74f981d8b75d423d66d696381a022fd73e8
John W. Fail <john@pixelache.ac>
2019-02-18 10:13:39 +0200

    hopefully closing rollbar #45

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
   
   def create
-    @projects = Project.published.search(params[:searchterm])
-    @news = Post.published.search(params[:searchterm])
-    @activities = Activity.published.search(params[:searchterm])
+    if params[:searchterm]
+      @projects = Project.published.search(params[:searchterm])
+      @news = Post.published.search(params[:searchterm])
+      @activities = Activity.published.search(params[:searchterm])
+    else
+      flash[:notice] = 'You must enter a search term.'
+      redirect_to '/'
+    end
     
   end
   
