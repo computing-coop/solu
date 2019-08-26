400d4bbbea1601fc61269e342cf5be2b3fd0dd5e
John W. Fail <john@pixelache.ac>
2019-08-26 11:34:36 +0300

    update themes_for_rails to prevent stack level recusion error

--- app/assets/themes/bioart/javascripts/application.js
+++ app/assets/themes/bioart/javascripts/application.js
@@ -8,7 +8,33 @@
 //= require foundation
 //= require bioart/javascripts/jquery.pageless
 
-$(function() { 
-  $(document).foundation();  
-
+$(document).on('ready page:load', function () {
+  $(document).foundation();
 });
+
+function toggleCalendar() {
+
+  if ( jQuery('#calendar_container').css("left") == "0px")  {
+    jQuery('#calendar_container').animate({left:"-85%"}, 600);
+    jQuery('#calendar_container').animate({top: parseInt($('#container').offset().top) + 'px' }, 600);
+
+    jQuery('#calendar_container').css('position', 'fixed');
+  } else {
+
+    var eTop = $('#calendar_container').offset().top;
+    jQuery('#calendar_container').css('position', 'absolute');
+    jQuery('#calendar_container').css('top', parseInt(eTop) + 'px');
+        jQuery('#calendar_container').animate({left:"0%"}, 100);
+  }
+
+}
+
+
+function scroll_To(target) {
+  $('html, body').stop().animate({
+      'scrollTop': $(target).offset().top - 40
+  }, 900, 'swing', function () {
+      //window.location.hash = target;
+  });
+  return false;
+}
