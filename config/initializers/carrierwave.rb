6c070a14e06b0629de399c7bbd5fc9a1f71e342c
John W. Fail <john@ptarmigan.fi>
2021-10-06 23:42:24 +0300

    trying to explicitly specify carrierwave cache dir

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   config.aws_bucket = Figaro.env.s3_bucket_name
   config.aws_acl    = :public_read
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-
+  config.cache_dir = File.join(Rails.root, 'tmp', 'uploads')
   config.aws_credentials = {
     access_key_id:     Figaro.env.s3_access_key,
     secret_access_key: Figaro.env.s3_secret_key, 
